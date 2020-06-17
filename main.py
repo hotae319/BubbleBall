35fece62f29cdfb3ca9ab8dfc512ccf6c366e46f
hotae <hotae319@gmail.com>
2020-06-16 23:57:13 -0700

    main script

---
+++ main.py
@@ -0,0 +1,35 @@
+from functions.path_planning import path_planning, re_planning
+import functions.get_keydirection as keydirection
+from functions.obj_class import Ball, metalBlock, woodBlock, powerUps
+from functions.parsing_movableobjects_levels import parsing_objects, run_simulation
+from functions.prediction import CheckCollisionWithFixed, ImpactMapBall2Fixed, ImpactMapBall2Wood
+'''
+0. import the enviornment setting
+'''
+level = 2
+id_grd, s_grd, s_total, id_total, n_total, movable_ID, ID_dict, ID_state_matching = parsing_objects(level)
+# s_total = [s_ball, s_flag, s_metal, s_wood, s_woodtri, s_speedupu, s_speedupd, s_speedupl, s_speedupr, s_slowdown, s_gravity, s_gravitydown, s_spring, s_teleport]
+# s_ball = [x,y,w,h,rot]
+'''
+1. path planning
+'''
+n_sample = 500
+map_size = [0,500,0,500]
+k1 = 6
+k2 = 10
+n_exclude = 1
+prm, shortest_path, sampling_list = path_planning(level, n_sample, k1 , map_size)    
+#re_planning(prm, level, shortest_path, k2, n_exclude)
+
+
+'''
+2. model prediction
+'''
+# Initialize
+ball = Ball(s_total[0][0],s_total[0][1],0,0)
+flag = s_total[1]
+# Check collision
+collision = CheckCollisionWithFixed(ball, s_grd)
+# Update
+if collision
+ball.update()
